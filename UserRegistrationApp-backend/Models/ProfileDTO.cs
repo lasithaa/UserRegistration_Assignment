437fd733932d360af5c29610804a1733890dcf41
Lasitha Akalanka <86360653+lasithaa@users.noreply.github.com>
2024-10-07 23:26:04 +0530

    backend

---
+++ UserRegistrationApp-backend/Models/ProfileDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace UserRegistrationApp.Models
+{
+    public class ProfileDTO
+    {
+        public int Id { get; set; }
+       
+        public string? ProfilePictureUrl { get; set; }
+        public string Email { get; set; }
+
+    }
+}
